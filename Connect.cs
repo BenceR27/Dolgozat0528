1c523e6c0786d59ede8270764d4e4fba72d4fdfa
Riz Bence <rizbence428@gmail.com>
2025-05-28 09:37:36 +0200

    everything works fine

---
+++ Connect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Dolgozat0528
+{
+    internal class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "rizb_filmek";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,98 @@
-﻿using System.Text;
+﻿using System.Security.Principal;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Dolgozat0528
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        internal static Connect Conn = new Connect();
+        public void lekerdez()
+        {
+            Conn.Connection.Open();
+
+            string sql = "SELECT * FROM `filmek` WHERE 1";
+
+            MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+
+            dr.Read();
+
+            do
+            {
+                var felhasznalo = new
+                {
+                    filmazon = dr.GetInt32(0),
+                    cim = dr.GetString(1),
+                    ev = dr.GetInt32(2),
+                    szines = dr.GetString(3),
+                    mufaj = dr.GetString(4),
+                    hossz = dr.GetInt32(5),
+                };
+
+                lbAdatok.Items.Add(felhasznalo.filmazon + ";" + felhasznalo.cim + ";" + felhasznalo.ev + ";" + felhasznalo.szines + ";" + felhasznalo.mufaj + ";" + felhasznalo.hossz);
+
+            }
+            while (dr.Read());
+
+            dr.Close();
+            Conn.Connection.Close();
+        }
+
+        private void lekerdez_Click(object sender, RoutedEventArgs e)
+        {
+            lekerdez();
+        }
+
+        private void lbAdatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbAdatok.SelectedItem == null)
+            {
+                return;
+
+            }
+            else
+            {
+            object sor = lbAdatok.SelectedItem;
+            string sorString = sor.ToString();
+            string[] felvag = sorString.Split(';');
+            lbFilmAzon.Content = felvag[0];
+            tb1.Text = felvag[1];
+            tb2.Text = felvag[2];
+            tb3.Text = felvag[3];
+            tb4.Text = felvag[4];
+            tb5.Text = felvag[5];
+            }
+        }
+        private void modosit_Click(object sender, RoutedEventArgs e)
+        {
+            Conn.Connection.Open();
+            string sql = "UPDATE `filmek` SET `cim`=@cim, `ev`=@ev, `szines`=@szines, `mufaj`=@mufaj, `hossz`=@hossz WHERE filmazon=@filmazon";
+
+            MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+            cmd.Parameters.AddWithValue("@cim", tb1.Text);
+            cmd.Parameters.AddWithValue("@ev", tb2.Text);
+            cmd.Parameters.AddWithValue("@szines", tb3.Text);
+            cmd.Parameters.AddWithValue("@mufaj", tb4.Text);
+            cmd.Parameters.AddWithValue("@hossz", tb5.Text);
+            cmd.Parameters.AddWithValue("@filmazon", lbFilmAzon.Content.ToString());
+
+            cmd.ExecuteNonQuery();
+            Conn.Connection.Close();
+
+            MessageBox.Show("Sikeres módosítás!");
+            lbAdatok.Items.Clear();
+            lekerdez();
+        }
+
     }
 }
